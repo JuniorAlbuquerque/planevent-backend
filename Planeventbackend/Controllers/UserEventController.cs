2516c74ff9712d708076e2caa943812ba4223336
JuniorAlbuquerque <junioralbuquerque81@gmail.com>
2020-12-30 04:32:39 -0300

    feat: create userevent controller

---
+++ Planeventbackend/Controllers/UserEventController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Planeventbackend.Data;
+using Planeventbackend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Planeventbackend.Controllers
+{
+    public class Associate
+    {
+        public string Email { get; set; }
+        public int EventId { get; set; }
+    }
+
+    [ApiController]
+    [Route("v1/userevent")]
+    public class UserEventController : ControllerBase
+    {
+        // Associate user to event
+        [HttpPost]
+        [Route("associate")]
+        public async Task<ActionResult<UserEventModel>>
+        Post([FromServices] DataContext context, [FromBody] Associate model )
+        {
+            var user = await context.users.FirstOrDefaultAsync(
+                x => x.Email == model.Email 
+            );
+
+            if (user != null)
+            {
+                var insert = new UserEventModel { Userid = user.Id, Eventid = model.EventId };
+                context.userEvents.Add(insert);
+                await context.SaveChangesAsync();
+
+                return Ok(new
+                {
+                    success = new
+                    {
+                        message = "Participante adicionado ao evento"
+                    }
+                });
+            }
+
+            else
+            {
+                return BadRequest(new
+                {
+                    error = new
+                    {
+                        message = "Participante não encontrado"
+                    }
+                });
+            }
+        }
+
+        // Get actually Events
+        [HttpGet]
+        [Route("actually/{id:int}")]
+        public async Task<ActionResult<List<EventModel>>>
+        Get([FromServices] DataContext context, int id)
+        {
+            var atual = DateTime.Now.Date;
+
+            var query =
+                await (from a in context.events
+                       join b in context.userEvents on a.Id equals b.Eventid
+                       join c in context.users on b.Userid equals c.Id
+                       where c.Id == id
+                       where a.Date <= atual
+                       select a).ToListAsync();
+
+            return query;
+        }
+
+        // Get Next Events
+        [HttpGet]
+        [Route("next/{id:int}")]
+        public async Task<ActionResult<List<EventModel>>>
+        GetNextEvents([FromServices] DataContext context, int id)
+        {
+            var atual = DateTime.Now.Date;
+
+            var query =
+                await (from a in context.events
+                       join b in context.userEvents on a.Id equals b.Eventid
+                       join c in context.users on b.Userid equals c.Id
+                       where c.Id == id
+                       where a.Date >= atual
+                       select a).ToListAsync();
+
+            return query;
+        }
+    }
+}
--- Planeventbackend/Models/UserEventModel.cs
+++ Planeventbackend/Models/UserEventModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,12 @@ namespace Planeventbackend.Models
 {
     public class UserEventModel
     {
+        [Required(ErrorMessage = "Este campo é obrigatório")]
         public int Userid { get; set; }
 
         public UserModel User { get; set; }
 
+        [Required(ErrorMessage = "Este campo é obrigatório")]
         public int Eventid { get; set; }
 
         public EventModel Event { get; set; }

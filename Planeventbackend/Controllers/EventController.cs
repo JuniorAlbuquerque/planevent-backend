50afa7c9542ef681247c2cb4169d30de734b9b14
JuniorAlbuquerque <junioralbuquerque81@gmail.com>
2020-12-30 04:33:33 -0300

    feat: add update and delete actions to event

--- Planeventbackend/Controllers/EventController.cs
+++ Planeventbackend/Controllers/EventController.cs
@@ -20,7 +20,7 @@ namespace Planeventbackend.Controllers
         public async Task<ActionResult<List<EventModel>>>
         Get([FromServices] DataContext context)
         {
-            var events = await context.events.ToListAsync();
+            var events = await context.events.OrderBy(x => x.Date).ToListAsync();
             return events;
         }
 
@@ -28,16 +28,18 @@ namespace Planeventbackend.Controllers
         [HttpGet]
         [Route("{id:int}")]
         public async Task<ActionResult<EventModel>>
-        Get([FromServices] DataContext context, int id)
+        GetById([FromServices] DataContext context, int id)
         {
             var resevent = await context.events
                 .AsNoTracking()
+                .Include(x => x.UserEventModels)
+                .ThenInclude(t => t.User)
                 .FirstOrDefaultAsync(x => x.Id == id);
 
             return resevent;
         }
 
-        // Get Event By User
+        // Get Event Created By User
         [HttpGet]
         [Route("user/{id:int}")]
         public async Task<ActionResult<List<EventModel>>>
@@ -45,7 +47,6 @@ namespace Planeventbackend.Controllers
         {
             var events = await context
                 .events
-                .AsNoTracking()
                 .Where(x => x.Userid == id)
                 .ToListAsync(); 
 
@@ -60,12 +61,82 @@ namespace Planeventbackend.Controllers
         {
             if (ModelState.IsValid)
             {
-                context.events.Add(model);
-                await context.SaveChangesAsync();
-                return model;
+                if (model.Type == "Exclusivo")
+                {
+                    var query = await context.events.FirstOrDefaultAsync(
+                        x => x.Date == model.Date && x.Type == "Exclusivo"  
+                    );
+
+                    if (query != null)
+                    {
+                        return BadRequest("Evento exclusivo na mesma data");
+                    } else
+                    {
+                        context.events.Add(model);
+                        await context.SaveChangesAsync();
+                        return model;
+                    }
+                } else
+                {
+                    context.events.Add(model);
+                    await context.SaveChangesAsync();
+                    return model;
+                }
+                
             } else
             {
                 return BadRequest(ModelState);
+            }
+        }
+
+        // Delete Event
+        [HttpDelete]
+        [Route("{id:int}")]
+        public async Task<ActionResult>
+        GetDelete([FromServices] DataContext context, int id)
+        {
+            var resevent = await context.events.FindAsync(id);
+            context.Entry(resevent).State = EntityState.Deleted;
+            await context.SaveChangesAsync();
+
+            return Ok(new
+            {
+                success = new
+                {
+                    message = "Evento excluido"
+                }
+            });
+        }
+
+        // Update Event
+        [HttpPut]
+        [Route("")]
+        public async Task<ActionResult>
+        Put([FromServices] DataContext context, [FromBody] EventModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var ev = await context.events.FindAsync(model.Id);
+                ev.Name = model.Name;
+                ev.Description = model.Description;
+                ev.Date = model.Date;
+                ev.Local = model.Local;
+                ev.Type = model.Type;
+
+                context.Entry(ev).State = EntityState.Modified;
+                await context.SaveChangesAsync();
+
+                return Ok(new
+                {
+                    success = new
+                    {
+                        message = "Dados atualizados com sucesso"
+                    }
+                });
+            }
+            else
+            {
+                return BadRequest();
             }
         }
     }
